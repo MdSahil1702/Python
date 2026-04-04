1aa73c399a862ead7ab4478bab2d9de9c59873bb
Md Sahil Siddiqui <sahilsiddiqui1702@gmail.com>
2026-04-04 18:46:16 +0530

    updating

--- Simple Projects/Personal_Expense_Tracker/main.py
+++ Simple Projects/Personal_Expense_Tracker/main.py
@@ -1,45 +1,45 @@
 
+from datetime import datetime
 
+# import os
 
-import os
-
-
-class Expenses:
-    id=10001
-    def __init__(self,date,category,amount,description):
-        self.date=date
-        self.category=category
-        self.amount=amount
-        self.description=description
-        self.Expense=[]
+# import datetime
+# class Expenses:
+#     id=10001
+#     def __init__(self,date,category,amount,description):
+#         self.date=date
+#         self.category=category
+#         self.amount=amount
+#         self.description=description
+#         self.Expense=[]
         
-        readFromFile(self.Expense)
+#         readFromFile(self.Expense)
         
-    def add_Expenses(self):
-        Expenses.id=Expenses.id+1
-        expense={
-        "id":Expenses.id,
-        "date":self.date,
-        "category":self.category,
-        "amount":self.amount,
-        "description":self.description,
+#     def add_Expenses(self):
+#         Expenses.id=Expenses.id+1
+#         expense={
+#         "id":Expenses.id,
+#         "date":self.date,
+#         "category":self.category,
+#         "amount":self.amount,
+#         "description":self.description,
         
-        }
+#         }
         
-        insertIntoFile(expense)
+#         insertIntoFile(expense)
         
-        # self.Expense.append(expense)
-        print("Expense added!")
-        # print(self.Expense)
+#         # self.Expense.append(expense)
+#         print("Expense added!")
+#         # print(self.Expense)
         
-    def view_Expenses(self):
+#     def view_Expenses(self):
         
-        # if(self.Expense.__sizeof__()==0):
-        if(os.path.exists("expenses_data.txt")):
-            readFromFile(self.Expense)
-            print(self.Expense)
-        else:
-            print("No expenses added till yet")
+#         # if(self.Expense.__sizeof__()==0):
+#         if(os.path.exists("expenses_data.txt")):
+#             readFromFile(self.Expense)
+#             print(self.Expense)
+#         else:
+#             print("No expenses added till yet")
         
         
         
@@ -49,16 +49,47 @@ class Expenses:
         
           
         
-def insertIntoFile(Expense):
-        f=open("expenses_data.txt",'a')
-        f.write(Expense)
-        f.close()
+# def insertIntoFile(Expense):
+#         f=open("expenses_data.txt",'a')
+#         f.write(Expense)
+#         f.close()
         
-def readFromFile(Expense):
-        if os.path.exists("expenses_data.txt"):
-            f=open("expenses_data.txt",'r')
-            Expense=f.read()
+# def readFromFile(Expense):
+#         if os.path.exists("expenses_data.txt"):
+#             f=open("expenses_data.txt",'r')
+#             Expense=f.read()
+        
+def validate_date(a):
+    if(a==""):
+        today=datetime.today().date()
+        return today
+    try:
+        valid_date=datetime.strptime(a,"%Y-%m-%d").date()
+        return valid_date
+    except ValueError:
+        print("Invalid date format! Please enter i YYYY-MM-DD format.")
             
+def add_expenses():
+    new_expense={}
+    
+    
+    a=input("Enter date (YYYY-MM-DD) or press Enter for today:")
+    a=validate_date(a)
+    new_expense["date"]=a
+    # print(new_expense["date"].strftime("%Y-%m-%d"))
+    
+    
+    
+    a=input("Select category (Food/Transport/Shopping/Bills/Other): ")
+    a.title()
+    if(a=="Food" or a=="Transport"or a=="Shopping"or a=="Bills"or a=="Other"):
+     new_expense["category"]=a
+    else:
+     print("Invalid category")
+    
+    a=int(input("Enter amount (in $): "))
+    
+    
             
 def main():
     
@@ -73,25 +104,28 @@ def main():
     
         match a :
             case 1:
-                print("Date (YYYY-MM-DD):")
-                b=input()
-                print("Category:")
-                b1=input()
-                print("Amount:")
-                b2=input()
-                print("Description:")
-                b3=input()
+                add_expenses()
                 
-                obj = Expenses(b,b1,b2,b3)
-                obj.add_Expenses()
+                # print("Date (YYYY-MM-DD):")
+                # b=input()
+                # print("Category:")
+                # b1=input()
+                # print("Amount:")
+                # b2=input()
+                # print("Description:")
+                # b3=input()
+                
+                # obj = Expenses(b,b1,b2,b3)
+                # obj.add_Expenses()
                 
             
                 
             case 2:
-                if(obj != None):
-                    obj.view_Expenses()
-                else:
-                    print("error")
+                # if(obj != None):
+                #     obj.view_Expenses()
+                # else:
+                #     print("error")
+                return
                     
             case 7:
                 print("Exiting!")
