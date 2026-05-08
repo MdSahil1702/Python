49a685b5e4d7914995b6acd5fd3cf3057fcd8c12
Md Sahil Siddiqui <sahilsiddiqui1702@gmail.com>
2026-05-08 09:51:11 +0530

    adding the stopwatch

--- Simple Projects/DigitalWatch/main.py
+++ Simple Projects/DigitalWatch/main.py
@@ -44,7 +44,43 @@ def setTime():
         if hr == 24:
             hr = 0
     
+
+def Timer():
+    print("Enter the timestamp")
+    hr=int(input("Enter the hour(0-24)"))
+    if hr>24 or hr<0:
+        print("Wrong input")
+        return
+    
+    min=int(input("Enter the minute"))
+    if min>60 or min<0:
+        print("Wrong input")
+        return
+    sec =60
+    while True:
         
+        os.system('cls')  
+       
+
+        print(f"{hr:02d}:{min:02d}:{sec:02d}")
+        
+        if hr == 0 and min == 0 and sec == 0:
+            print("\nTime Up!")
+            break
+        time.sleep(1)
+        sec -= 1
+
+       
+
+        if sec < 0:
+            sec = 59
+            min -= 1
+
+        if min < 0:
+            min = 59
+            hr -= 1
+             
+      
                 
         
             
