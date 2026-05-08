3f61fd82f7f0b98e311f03231106deb4326a64c9
Md Sahil Siddiqui <sahilsiddiqui1702@gmail.com>
2026-05-08 08:26:15 +0530

    win loss tie logic

--- Simple Projects/rockpaperscissor.py/main.py
+++ Simple Projects/rockpaperscissor.py/main.py
@@ -1,18 +1,39 @@
 import random
-print("===========WELCOME TO ROCK PAPER SCISSOR GAME===========")
 
-
-user_select= int(input("Select 1.Rock\n 2.Paper\n 3.Scissor\n"))
-user_select=converToString(user_select)
-computer_select= random.choice(["Rock","Paper","Scissor"])
-
-
-
-    
 def converToString(userinput):
     if userinput==1:
-        return "Rock"
+        return "rock"
     elif userinput==2:
-        return "Paper"
-    return "Scissor"
+        return "paper"
+    return "scissor"
+
+
+def win_loss(user,computer):
+    if user=="rock" and computer=="paper":
+        print("Computer Win!!")
+    elif user=="paper" and computer=="Scissor":
+        print("Computer Win!!")
+    elif user=="scissor" and computer == "rock":
+        print("Computer Win!!!")
+    elif user==computer:
+        print("Match Tie!!")
+    else:
+        print("You Win!!")
+
+
+
+
+print("===========WELCOME TO ROCK PAPER SCISSOR GAME===========")
+
+def run():
+    user_select= int(input("Select\n 1.Rock\n 2.Paper\n 3.Scissor\n 4.Exit\n"))
+    user=converToString(user_select)
+    computer_select= random.choice(["rock","paper","scissor"])
+    win_loss(user,computer_select)
+
+while 1:
+    run()
+
+    
+
 
